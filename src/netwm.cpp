KDE/kwindowsystem

commit 59cb063b99e6ec13c0bc3cf3fd95a01258184e86

Add support for protocols to NETWinInfo
NETWinInfo is now able to read all known protocols set on WM_PROTOCOLS
property and provides the set protocols as flags.

So far the following protocols are supported:
* WM_TAKE_FOCUS (ICCCM 4.1.2.7)
* WM_DELETE_WINDOW (ICCCM 4.1.2.7)
* _NET_WM_SYNC_REQUEST (EWMH)
* _NET_WM_PING (EWMH)
* _NET_WM_CONTEXT_HELP (KDE specific extension to EWMH)

REVIEW: 120213
CHANGELOG: NETWinInfo provides convenient wrapper for WM_PROTOCOLS.

src/netwm.cpp
@@ -118,7 +118,11 @@ static xcb_atom_t kde_net_wm_shadow                 = 0;
 
 // application protocols
 static xcb_atom_t wm_protocols = 0;
+static xcb_atom_t wm_take_focus = 0;
+static xcb_atom_t wm_delete_window = 0;
 static xcb_atom_t net_wm_ping = 0;
+static xcb_atom_t net_wm_sync_request = 0;
+static xcb_atom_t net_wm_context_help = 0;
 
 // application window types
 static xcb_atom_t net_wm_window_type_normal  = 0;
@@ -382,7 +386,7 @@ static QByteArray get_atom_name(xcb_connection_t *c, xcb_atom_t atom)
 }
 #endif
 
-static const int netAtomCount = 88;
+static const int netAtomCount = 91;
 
 static void create_netwm_atoms(xcb_connection_t *c)
 {
@@ -481,6 +485,10 @@ static void create_netwm_atoms(xcb_connection_t *c)
 
         { "WM_STATE",                             &xa_wm_state                      },
         { "WM_PROTOCOLS",                         &wm_protocols                     },
+        { "WM_TAKE_FOCUS",                        &wm_take_focus                    },
+        { "WM_DELETE_WINDOW",                     &wm_delete_window                 },
+        { "_NET_WM_SYNC_REQUEST",                 &net_wm_sync_request              },
+        { "_NET_WM_CONTEXT_HELP",                 &net_wm_context_help              },
 
         { "_NET_WM_FULL_PLACEMENT",               &net_wm_full_placement            },
         { "_KDE_NET_WM_ACTIVITIES",               &kde_net_wm_activities            },
@@ -2765,6 +2773,7 @@ NETWinInfo::NETWinInfo(xcb_connection_t *connection, xcb_window_t window, xcb_wi
     p->blockCompositing = false;
     p->urgency = false;
     p->input = true;
+    p->protocols = NET::NoProtocol;
 
     // p->strut.left = p->strut.right = p->strut.top = p->strut.bottom = 0;
     // p->frame_strut.left = p->frame_strut.right = p->frame_strut.top =
@@ -2826,6 +2835,7 @@ NETWinInfo::NETWinInfo(xcb_connection_t *connection, xcb_window_t window, xcb_wi
     p->blockCompositing = false;
     p->urgency = false;
     p->input = true;
+    p->protocols = NET::NoProtocol;
 
     // p->strut.left = p->strut.right = p->strut.top = p->strut.bottom = 0;
     // p->frame_strut.left = p->frame_strut.right = p->frame_strut.top =
@@ -3937,6 +3947,8 @@ void NETWinInfo::event(xcb_generic_event_t *event, NET::Properties *properties, 
             dirty2 |= WM2BlockCompositing;
         } else if (pe->atom == kde_net_wm_shadow) {
             dirty2 |= WM2KDEShadow;
+        } else if (pe->atom == wm_protocols) {
+            dirty2 |= WM2Protocols;
         }
 
         do_update = true;
@@ -4113,6 +4125,10 @@ void NETWinInfo::update(NET::Properties dirtyProperties, NET::Properties2 dirtyP
 
     if (dirty2 & WM2ClientMachine) {
         cookies[c++] = xcb_get_property(p->conn, false, p->window, XCB_ATOM_WM_CLIENT_MACHINE, XCB_ATOM_STRING, 0, MAX_PROP_SIZE);
+    }
+
+    if (dirty2 & WM2Protocols) {
+        cookies[c++] = xcb_get_property(p->conn, false, p->window, wm_protocols, XCB_ATOM_ATOM, 0, 2048);
     }
 
     c = 0;
@@ -4605,6 +4621,24 @@ void NETWinInfo::update(NET::Properties dirtyProperties, NET::Properties2 dirtyP
             p->client_machine = nstrndup(value.constData(), value.length());
         }
     }
+
+    if (dirty2 & WM2Protocols) {
+        const QVector<xcb_atom_t> protocols = get_array_reply<xcb_atom_t>(p->conn, cookies[c++], XCB_ATOM_ATOM);
+        p->protocols = NET::NoProtocol;
+        for (auto it = protocols.begin(); it != protocols.end(); ++it) {
+            if ((*it) == wm_take_focus) {
+                p->protocols |= TakeFocusProtocol;
+            } else if ((*it) == wm_delete_window) {
+                p->protocols |= DeleteWindowProtocol;
+            } else if ((*it) == net_wm_ping) {
+                p->protocols |= PingProtocol;
+            } else if ((*it) == net_wm_sync_request) {
+                p->protocols |= SyncRequestProtocol;
+            } else if ((*it) == net_wm_context_help) {
+                p->protocols |= ContextHelpProtocol;
+            }
+        }
+    }
 }
 
 NETRect NETWinInfo::iconGeometry() const
@@ -4843,6 +4877,16 @@ NET::Properties2 NETWinInfo::passedProperties2() const
 NET::MappingState NETWinInfo::mappingState() const
 {
     return p->mapping_state;
+}
+
+NET::Protocols NETWinInfo::protocols() const
+{
+    return p->protocols;
+}
+
+bool NETWinInfo::supportsProtocol(NET::Protocol protocol) const
+{
+    return p->protocols.testFlag(protocol);
 }
 
 void NETRootInfo::virtual_hook(int, void *)

autotests/netwininfotestclient.cpp
@@ -84,6 +84,8 @@ private Q_SLOTS:
     void testInput_data();
     void testInput();
     void testTransientFor();
+    void testProtocols_data();
+    void testProtocols();
 
 private:
     void performNameTest(xcb_atom_t atom, const char *(NETWinInfo:: *getter)(void)const, void (NETWinInfo:: *setter)(const char *), NET::Property property);
@@ -836,6 +838,127 @@ void NetWinInfoTestClient::testActivities()
     // only updated after event
     waitForPropertyChange(&info, atom, NET::Property(0), NET::WM2Activities);
     QTEST(QByteArray(info.activities()), "expectedActivities");
+}
+
+Q_DECLARE_METATYPE(NET::Protocols)
+void NetWinInfoTestClient::testProtocols_data()
+{
+    QTest::addColumn<NET::Protocols>("protocols");
+    QTest::addColumn<bool>("takeFocus");
+    QTest::addColumn<bool>("deleteWindow");
+    QTest::addColumn<bool>("ping");
+    QTest::addColumn<bool>("sync");
+    QTest::addColumn<bool>("context");
+
+    const NET::Protocol t = NET::TakeFocusProtocol;
+    const NET::Protocol d = NET::DeleteWindowProtocol;
+    const NET::Protocol p = NET::PingProtocol;
+    const NET::Protocol s = NET::SyncRequestProtocol;
+    const NET::Protocol c = NET::ContextHelpProtocol;
+
+    QTest::newRow("none") << NET::Protocols(NET::NoProtocol) << false << false << false << false << false;
+
+    QTest::newRow("t") << NET::Protocols(t) << true << false << false << false << false;
+    QTest::newRow("d") << NET::Protocols(d) << false << true << false << false << false;
+    QTest::newRow("p") << NET::Protocols(p) << false << false << true << false << false;
+    QTest::newRow("s") << NET::Protocols(s) << false << false << false << true << false;
+    QTest::newRow("c") << NET::Protocols(c) << false << false << false << false << true;
+
+    // all two combinations with t
+    QTest::newRow("t/d") << NET::Protocols(t | d) << true << true  << false << false << false;
+    QTest::newRow("t/p") << NET::Protocols(t | p) << true << false << true  << false << false;
+    QTest::newRow("t/s") << NET::Protocols(t | s) << true << false << false << true  << false;
+    QTest::newRow("t/c") << NET::Protocols(t | c) << true << false << false << false << true;
+    // all two combinations with d
+    QTest::newRow("d/p") << NET::Protocols(d | p) << false << true << true  << false << false;
+    QTest::newRow("d/s") << NET::Protocols(d | s) << false << true << false << true  << false;
+    QTest::newRow("d/c") << NET::Protocols(d | c) << false << true << false << false << true;
+    // all two combinations with p
+    QTest::newRow("p/s") << NET::Protocols(p | s) << false << false << true << true  << false;
+    QTest::newRow("p/c") << NET::Protocols(p | c) << false << false << true << false << true;
+    // and remaining two combination
+    QTest::newRow("s/c") << NET::Protocols(s | c) << false << false << false << true << true;
+
+    // all three combinations with t
+    QTest::newRow("t/d/p") << NET::Protocols(t | d | p) << true << true  << true  << false << false;
+    QTest::newRow("t/d/s") << NET::Protocols(t | d | s) << true << true  << false << true  << false;
+    QTest::newRow("t/d/c") << NET::Protocols(t | d | c) << true << true  << false << false << true;
+    QTest::newRow("t/p/s") << NET::Protocols(t | p | s) << true << false << true  << true  << false;
+    QTest::newRow("t/p/c") << NET::Protocols(t | p | c) << true << false << true  << false << true;
+    QTest::newRow("t/s/c") << NET::Protocols(t | s | c) << true << false << false << true  << true;
+    // all three combinations with d
+    QTest::newRow("d/p/s") << NET::Protocols(d | p | s) << false << true << true  << true  << false;
+    QTest::newRow("d/p/c") << NET::Protocols(d | p | c) << false << true << true  << false << true;
+    QTest::newRow("d/s/c") << NET::Protocols(d | s | c) << false << true << false << true  << true;
+    // and remaining
+    QTest::newRow("p/s/c") << NET::Protocols(p | s | c) << false << false << true << true << true;
+
+    QTest::newRow("t/d/p/s") << NET::Protocols(t | d | p | s) << true  << true  << true  << true  << false;
+    QTest::newRow("t/d/p/c") << NET::Protocols(t | d | p | c) << true  << true  << true  << false << true;
+    QTest::newRow("t/d/s/c") << NET::Protocols(t | d | s | c) << true  << true  << false << true  << true;
+    QTest::newRow("t/p/s/c") << NET::Protocols(t | p | s | c) << true  << false << true  << true  << true;
+    QTest::newRow("d/p/s/c") << NET::Protocols(d | p | s | c) << false << true  << true  << true  << true;
+
+    QTest::newRow("all") << NET::Protocols(t | d | p | s | c) << true << true << true << true << true;
+}
+
+void NetWinInfoTestClient::testProtocols()
+{
+    QVERIFY(connection());
+    ATOM(WM_PROTOCOLS)
+    KXUtils::Atom takeFocus(connection(), QByteArrayLiteral("WM_TAKE_FOCUS"));
+    KXUtils::Atom deleteWindow(connection(), QByteArrayLiteral("WM_DELETE_WINDOW"));
+    KXUtils::Atom ping(connection(), QByteArrayLiteral("_NET_WM_PING"));
+    KXUtils::Atom syncRequest(connection(), QByteArrayLiteral("_NET_WM_SYNC_REQUEST"));
+    KXUtils::Atom contextHelp(connection(), QByteArrayLiteral("_NET_WM_CONTEXT_HELP"));
+    INFO
+
+    QVERIFY(!info.supportsProtocol(NET::TakeFocusProtocol));
+    QVERIFY(!info.supportsProtocol(NET::DeleteWindowProtocol));
+    QVERIFY(!info.supportsProtocol(NET::PingProtocol));
+    QVERIFY(!info.supportsProtocol(NET::SyncRequestProtocol));
+    QVERIFY(!info.supportsProtocol(NET::ContextHelpProtocol));
+    QCOMPARE(info.protocols(), NET::Protocols(NET::NoProtocol));
+
+    QVector<xcb_atom_t> props;
+    QFETCH(NET::Protocols, protocols);
+    if (protocols.testFlag(NET::TakeFocusProtocol)) {
+        props << takeFocus;
+    }
+    if (protocols.testFlag(NET::DeleteWindowProtocol)) {
+        props << deleteWindow;
+    }
+    if (protocols.testFlag(NET::PingProtocol)) {
+        props << ping;
+    }
+    if (protocols.testFlag(NET::SyncRequestProtocol)) {
+        props << syncRequest;
+    }
+    if (protocols.testFlag(NET::ContextHelpProtocol)) {
+        props << contextHelp;
+    }
+
+    xcb_change_property(connection(), XCB_PROP_MODE_REPLACE, m_testWindow, atom, XCB_ATOM_ATOM, 32, props.size(), props.constData());
+    xcb_flush(connection());
+
+    // only updated after event
+    waitForPropertyChange(&info, atom, NET::Property(0), NET::WM2Protocols);
+    QCOMPARE(info.protocols(), protocols);
+    QTEST(info.supportsProtocol(NET::TakeFocusProtocol), "takeFocus");
+    QTEST(info.supportsProtocol(NET::DeleteWindowProtocol), "deleteWindow");
+    QTEST(info.supportsProtocol(NET::PingProtocol), "ping");
+    QTEST(info.supportsProtocol(NET::SyncRequestProtocol), "sync");
+    QTEST(info.supportsProtocol(NET::ContextHelpProtocol), "context");
+
+    xcb_delete_property(connection(), m_testWindow, atom);
+    xcb_flush(connection());
+    waitForPropertyChange(&info, atom, NET::Property(0), NET::WM2Protocols);
+    QVERIFY(!info.supportsProtocol(NET::TakeFocusProtocol));
+    QVERIFY(!info.supportsProtocol(NET::DeleteWindowProtocol));
+    QVERIFY(!info.supportsProtocol(NET::PingProtocol));
+    QVERIFY(!info.supportsProtocol(NET::SyncRequestProtocol));
+    QVERIFY(!info.supportsProtocol(NET::ContextHelpProtocol));
+    QCOMPARE(info.protocols(), NET::Protocols(NET::NoProtocol));
 }
 
 QTEST_MAIN(NetWinInfoTestClient)

src/netwm.h
@@ -1519,6 +1519,19 @@ public:
     NET::Properties event(xcb_generic_event_t *event);
 
     /**
+     * @returns The window manager protocols this Client supports.
+     * @since 5.3
+     **/
+    NET::Protocols protocols() const;
+
+    /**
+     * @returns @c true if the Client supports the @p protocol.
+     * @param protocol The window manager protocol to test for
+     * @since 5.3
+     **/
+    bool supportsProtocol(NET::Protocol protocol) const;
+
+    /**
        Sentinel value to indicate that the client wishes to be visible on
        all desktops.
 

src/netwm_def.h
@@ -677,6 +677,7 @@ public:
         @li WM2FullscreenMonitors _NET_WM_FULLSCREEN_MONITORS
         @li WM2Urgency urgency hint in WM_HINTS (see ICCCM 4.1.2.4)
         @li WM2Input input hint (input in WM_HINTS, see ICCCM 4.1.2.4)
+        @li WM2Protocols see NET::Protocol
     **/
     enum Property2 {
         WM2UserTime            = 1u << 0,
@@ -702,6 +703,7 @@ public:
         WM2KDEShadow           = 1u << 20, // NOT Standard @since 4.7
         WM2Urgency             = 1u << 21, // @since 5.3
         WM2Input               = 1u << 22, // @since 5.3
+        WM2Protocols           = 1u << 23, // @since 5.3
         WM2AllProperties       = ~0u
     };
     Q_DECLARE_FLAGS(Properties2, Property2)
@@ -750,6 +752,22 @@ public:
     };
 
     /**
+     * Protocols supported by the client.
+     * See ICCCM 4.1.2.7.
+     *
+     * @since 5.3
+     **/
+    enum Protocol {
+        NoProtocol = 0,
+        TakeFocusProtocol = 1 << 0, ///< WM_TAKE_FOCUS
+        DeleteWindowProtocol = 1 << 1, ///< WM_DELETE_WINDOW
+        PingProtocol = 1 << 2, ///< _NET_WM_PING from EWMH
+        SyncRequestProtocol = 1 << 3, ///< _NET_WM_SYNC_REQUEST from EWMH
+        ContextHelpProtocol = 1 << 4 ///< _NET_WM_CONTEXT_HELP, NON STANDARD!
+    };
+    Q_DECLARE_FLAGS(Protocols, Protocol)
+
+    /**
      Compares two X timestamps, taking into account wrapping and 64bit architectures.
      Return value is like with strcmp(), 0 for equal, -1 for time1 < time2, 1 for time1 > time2.
     */
@@ -767,5 +785,6 @@ Q_DECLARE_OPERATORS_FOR_FLAGS(NET::Properties2)
 Q_DECLARE_OPERATORS_FOR_FLAGS(NET::WindowTypes)
 Q_DECLARE_OPERATORS_FOR_FLAGS(NET::States)
 Q_DECLARE_OPERATORS_FOR_FLAGS(NET::Actions)
+Q_DECLARE_OPERATORS_FOR_FLAGS(NET::Protocols)
 
 #endif // netwm_def_h

src/netwm_p.h
@@ -167,6 +167,7 @@ struct NETWinInfoPrivate {
     bool blockCompositing;
     bool urgency;
     bool input;
+    NET::Protocols protocols;
 
     int ref;
 };

commit 3576df163c8c64ebba74fc51c95de900dbf0220e

Fix incorrect count of netwm atoms
This fixes a regression introduced with
59cb063b99e6ec13c0bc3cf3fd95a01258184e86.

The commit added 4 new atoms but only incremented the atom count by 3.
Which can cause a crash as soon as the NET classes are used.

To ensure that such an error is not introduced again, an assert is
added.

BUG: 339771
REVIEW: 120539

src/netwm.cpp
@@ -386,7 +386,7 @@ static QByteArray get_atom_name(xcb_connection_t *c, xcb_atom_t atom)
 }
 #endif
 
-static const int netAtomCount = 91;
+static const int netAtomCount = 92;
 
 static void create_netwm_atoms(xcb_connection_t *c)
 {
@@ -498,6 +498,7 @@ static void create_netwm_atoms(xcb_connection_t *c)
     };
 
     assert(!netwm_atoms_created);
+    assert(sizeof(atoms)/sizeof(atoms[0]) == netAtomCount + 1);
 
     // Send the intern atom requests
     xcb_intern_atom_cookie_t cookies[netAtomCount];