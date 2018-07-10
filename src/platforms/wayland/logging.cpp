f71728c9010594c1dbf046ab56b023c40879c911
Laurent Montel <montel@kde.org>
2018-07-10 07:49:45 +0200

    Autogenerate loggin category

--- src/platforms/wayland/logging.cpp
+++
@@ -1,2 +0,0 @@
-#include "logging.h"
-Q_LOGGING_CATEGORY(KWAYLAND_KWS, "org.kde.kf5.kwindowsystem.kwayland", QtWarningMsg)
--- src/platforms/wayland/logging.h
+++
@@ -1,8 +0,0 @@
-#ifndef LOGGING_H
-#define LOGGING_H
-
-#include <QDebug>
-#include <QLoggingCategory>
-Q_DECLARE_LOGGING_CATEGORY(KWAYLAND_KWS)
-
-#endif
