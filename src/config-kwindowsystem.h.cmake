09b43cd725bda57b947e2a36aec01b5ba4c917cd
Nicolas Fella <nicolas.fella@gmx.de>
2021-08-10 01:14:27 +0200

    Remove unused xrender dependency

--- src/config-kwindowsystem.h.cmake
+++ src/config-kwindowsystem.h.cmake
@@ -6,8 +6,5 @@
 /* Define to 1 if you have the Xfixes library */
 #cmakedefine01 KWINDOWSYSTEM_HAVE_XFIXES
 
-/* Define to 1 if you have the Xrender library */
-#cmakedefine01 KWINDOWSYSTEM_HAVE_XRENDER
-
 /* Path to xcb plugin */
 #define XCB_PLUGIN_PATH "${KDE_INSTALL_FULL_PLUGINDIR}/kf5/kwindowsystem/KF5WindowSystemX11Plugin.so"
