2b2e4b6d1f21d6c6a5432374bfc9af58eeca52ec
David Edmundson <kde@davidedmundson.co.uk>
2023-03-17 17:32:18 +0000

    QML: Use correct registration type for existing singleton
    qmlRegisterSingletonType will be invoked once per engine, but more
    importantly try to clean itself up on destruction.
    
    We want to register the singleton instance we have already.

--- src/qml/kwindowsystemplugin.cpp
+++ src/qml/kwindowsystemplugin.cpp
@@ -21,12 +21,8 @@ void KWindowSystemPlugin::registerTypes(const char *uri)
     Q_ASSERT(QString::fromLatin1(uri) == QLatin1String("org.kde.kwindowsystem"));
 
 #if KWINDOWSYSTEM_HAVE_X11
-    qmlRegisterSingletonType<KX11Extras>("org.kde.kwindowsystem", 1, 0, "KX11Extras", [](QQmlEngine *, QJSEngine *) -> QObject * {
-        return KX11Extras::self();
-    });
+    qmlRegisterSingletonInstance<KX11Extras>("org.kde.kwindowsystem", 1, 0, "KX11Extras", KX11Extras::self());
 #endif
 
-    qmlRegisterSingletonType<KWindowSystem>("org.kde.kwindowsystem", 1, 0, "KWindowSystem", [](QQmlEngine *, QJSEngine *) -> QObject * {
-        return KWindowSystem::self();
-    });
+    qmlRegisterSingletonInstance<KWindowSystem>("org.kde.kwindowsystem", 1, 0, "KWindowSystem", KWindowSystem::self());
 }
