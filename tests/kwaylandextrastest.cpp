e3decd445f34fad9aff4bc79e346fe47e2cdd2a1
Kai Uwe Broulik <kde@privat.broulik.de>
2023-12-27 08:59:30 +0100

    kwaylandextrastest: Add test for (un)exporting a window
    Prints the XDG foreign handle which you can then copy
    and use in another application for testing.

--- tests/kwaylandextrastest.cpp
+++ tests/kwaylandextrastest.cpp
@@ -23,8 +23,14 @@ public:
 private:
     void updateSerial();
     void requestToken();
+
+    void exportWindow();
+    void unexportWindow();
+    void setExportedHandle(const QString &handle);
+
     QLabel *m_serialLabel;
     QLabel *m_tokenLabel;
+    QLabel *m_exportedLabel;
 };
 
 Window::Window()
@@ -42,11 +48,28 @@ Window::Window()
     m_tokenLabel->setTextInteractionFlags(Qt::TextSelectableByMouse | Qt::TextSelectableByKeyboard);
     m_tokenLabel->setText("XDG actvation token:");
 
+    QHBoxLayout *exportLayout = new QHBoxLayout;
+
+    QPushButton *exportButton = new QPushButton("Export window");
+    connect(exportButton, &QPushButton::clicked, this, &Window::exportWindow);
+
+    QPushButton *unexportButton = new QPushButton("Unexport window");
+    connect(unexportButton, &QPushButton::clicked, this, &Window::unexportWindow);
+
+    m_exportedLabel = new QLabel;
+    m_exportedLabel->setTextInteractionFlags(Qt::TextSelectableByMouse | Qt::TextSelectableByKeyboard);
+    setExportedHandle(QString());
+
     QVBoxLayout *layout = new QVBoxLayout(this);
     layout->addWidget(serialButton);
     layout->addWidget(m_serialLabel);
     layout->addWidget(tokenButton);
     layout->addWidget(m_tokenLabel);
+
+    exportLayout->addWidget(exportButton);
+    exportLayout->addWidget(unexportButton);
+    layout->addLayout(exportLayout);
+    layout->addWidget(m_exportedLabel);
 }
 
 void Window::updateSerial()
@@ -66,6 +89,31 @@ void Window::requestToken()
         Qt::SingleShotConnection);
 
     KWaylandExtras::requestXdgActivationToken(windowHandle(), KWaylandExtras::self()->lastInputSerial(windowHandle()), QString());
+}
+
+void Window::exportWindow()
+{
+    connect(
+        KWaylandExtras::self(),
+        &KWaylandExtras::windowExported,
+        this,
+        [this](QWindow *window, const QString &handle) {
+            Q_UNUSED(window);
+            setExportedHandle(handle);
+        },
+        Qt::SingleShotConnection);
+    KWaylandExtras::exportWindow(windowHandle());
+}
+
+void Window::unexportWindow()
+{
+    KWaylandExtras::unexportWindow(windowHandle());
+    setExportedHandle(QString());
+}
+
+void Window::setExportedHandle(const QString &handle)
+{
+    m_exportedLabel->setText("XDG foreign handle: " + handle);
 }
 
 int main(int argc, char **argv)
